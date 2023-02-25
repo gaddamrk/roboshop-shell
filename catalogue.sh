df1d1a3904a6913be03564effd9f1cd93894382e
raj <raj>
2023-02-25 12:25:40 +0530

    catalogue script changes

--- catalogue.sh
+++ catalogue.sh
@@ -20,3 +20,8 @@ cp $script_location/files/catalogue.service /etc/systemd/system/catalogue.servic
 systemctl daemon-reload
 systemctl enable catalogue
 systemctl start catalogue
+
+cp $script_location/files/mongodb.repo /etc/yum.repos.d/mongodb.repo
+yum install mongodb-org-shell -y
+
+mongo --host localhost </app/schema/catalogue.js
