81614152a6f2b0abe9d0682b8827c27d8329392d
raj <raj>
2023-02-27 08:46:27 +0530

    catalogue

--- catalogue.sh
+++ catalogue.sh
@@ -2,7 +2,7 @@ script_location=$(pwd)
 
 # setup nodeJS repos. Vendor is providing a script to setup the repos.
 
-set -e
+
 
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 yum install nodejs -y
