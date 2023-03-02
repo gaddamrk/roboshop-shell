1b1a4bddaff1eee9b7ed2f1005ac1bfc91cd43eb
raj <raj>
2023-03-02 20:31:27 +0530

    common.sh

--- mongodb.sh
+++ mongodb.sh
@@ -1,25 +1,25 @@
 source common.sh
 
 print_head "copy mongo repo files"
-cp ${script_location}/files/mongodb.repo /etc/yum.repos.d/mongo.repo
+cp ${script_location}/files/mongodb.repo /etc/yum.repos.d/mongo.repo &>>${log}
 status_check
 
 print_head "install mongodb"
-yum install mongodb-org -y
+yum install mongodb-org -y &>>${log}
 status_check
 
 print_head "enable mongodb"
-systemctl enable mongod
+systemctl enable mongod &>>${log}
 status_check
 
 print_head "start mongod "
-systemctl start mongod
+systemctl start mongod &>>${log}
 status_check
 
 print_head "update mongod listen address"
-sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>>${log}
 status_check
 
 print_head "restart mongod"
-systemctl restart mongod
+systemctl restart mongod &>>${log}
 status_check
