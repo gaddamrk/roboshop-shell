f605dc6ddb1d05760c533b1cb041d26cdaccac7e
raj <raj>
2023-03-02 12:44:04 +0530

    if data

--- catalogue.sh
+++ catalogue.sh
@@ -4,7 +4,7 @@ log=/tmp/roboshop.log
 status_check() {
   if [ $? -eq 0 ]
   then
-    echo -e "\e[35m success \e[0m"
+    echo -e "\e[32m success \e[0m"
   else
     echo -e "\e[31m failure \e[0m"
     echo refer log file more information log - ${log}
