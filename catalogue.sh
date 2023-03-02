fb2d5a90138ec563698b81660f2d6555fb35df58
raj <raj>
2023-03-02 13:08:37 +0530

    common.sh

--- catalogue.sh
+++ catalogue.sh
@@ -1,16 +1,4 @@
-script_location=$(pwd)
-log=/tmp/roboshop.log
-
-status_check() {
-  if [ $? -eq 0 ]
-  then
-    echo -e "\e[32m success \e[0m"
-  else
-    echo -e "\e[31m failure \e[0m"
-    echo refer log file more information log - ${log}
-  exit
-  fi
-}
+source common.sh
 
 # setup nodeJS repos. Vendor is providing a script to setup the repos.
 
--- frontend.sh
+++ frontend.sh
@@ -1,61 +1,32 @@
-script_location=$(pwd)
-log=/tmp/roboshop.log
+source common.sh
 
 echo -e "\e[35m install nginx \e[0m"
 yum install nginx -y &>>${log}
-if [ $? -eq 0 ]; then
-  echo success
-else
-  echo failure
-fi
+status_check
+
 echo -e "\e[35m remove old content\e[0m"
 rm -rf /usr/share/nginx/html/*  &>>${log}
-if [ $? -eq 0 ]
-then
-  echo success
-else
-  echo failure
-fi
+status_check
+
 echo -e "\e[35m download the frontend content\e[0m"
 curl -o /tmp/frontend.zip http://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${log}
-if [ $? -eq 0 ]
-then
-  echo success
-else
-  echo failure
-fi
+status_check
+
 cd /usr/share/nginx/html
-echo -e "\e[35m extract the frontend file\e[0ms"
+echo -e "\e[35m extract the frontend files\e[0ms"
 unzip /tmp/frontend.zip &>>${log}
-if [ $? -eq 0 ]
-then
-  echo success
-else
-  echo failure
-fi
+status_check
+
 echo -e "\e[35m copy the roboshop content file\e[0m"
 cp $script_location/files/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf &>>${log}
-if [ $? -eq 0 ]
-then
-  echo success
-else
-  echo failure
-fi
-echo -e "\e[35m efnable nginx\e[0m"
+status_check
+
+echo -e "\e[35m efnable nginx service \e[0m"
 systemctl enable nginx &>>${log}
-if [ $? -eq 0 ]
-then
-  echo success
-else
-  echo failure
-fi
-echo -e "\e[35m start nginx\e[0m"
+status_check
+
+echo -e "\e[35m start nginx service\e[0m"
 systemctl restart nginx &>>${log}
-if [ $? -eq 0 ]
-then
-  echo success
-else
-  echo failure
-fi
+status_check
 
 
