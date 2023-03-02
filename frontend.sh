fe30580923c29e206e779c69f907320d98e50e17
raj <raj>
2023-03-02 09:18:11 +0530

    if data

--- frontend.sh
+++ frontend.sh
@@ -7,7 +7,7 @@ if [ $? -eq 0 ]; then
   echo success
 else
   echo failure
-
+fi
 echo -e "\e[35m remove old content\e[0m"
 rm -rf /usr/share/nginx/html/*  &>>${log}
 if [ $? -eq 0 ]
@@ -15,7 +15,7 @@ then
   echo success
 else
   echo failure
-
+fi
 echo -e "\e[35m download the frontend content\e[0m"
 curl -o /tmp/frontend.zip http://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${log}
 if [ $? -eq 0 ]
@@ -23,7 +23,7 @@ then
   echo success
 else
   echo failure
-
+fi
 cd /usr/share/nginx/html
 echo -e "\e[35m extract the frontend file\e[0ms"
 unzip /tmp/frontend.zip &>>${log}
@@ -32,7 +32,7 @@ then
   echo success
 else
   echo failure
-
+fi
 echo -e "\e[35m copy the roboshop content file\e[0m"
 cp $script_location/files/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf &>>${log}
 if [ $? -eq 0 ]
@@ -40,7 +40,7 @@ then
   echo success
 else
   echo failure
-
+fi
 echo -e "\e[35m efnable nginx\e[0m"
 systemctl enable nginx &>>${log}
 if [ $? -eq 0 ]
@@ -48,7 +48,7 @@ then
   echo success
 else
   echo failure
-
+fi
 0echo -e "\e[35m start nginx\e[0m"
 systemctl restart nginx &>>${log}
 if [ $? -eq 0 ]
@@ -56,6 +56,6 @@ then
   echo success
 else
   echo failure
-
+fi
 
 
