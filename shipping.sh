a0d7bddbda8058537c62dc3e4acd21aca102e913
raj <raj>
2023-03-05 19:25:28 +0530

    mysql

--- shipping.sh
+++ shipping.sh
@@ -4,7 +4,7 @@ if [ -z "${root_mysql_password}" ]; then
    echo "vairable root_mysql_password is needed"
    exit
 fi
-component=catalogue
+component=shipping
 schema_load=true
 schema_type=mysql
 nodejs
